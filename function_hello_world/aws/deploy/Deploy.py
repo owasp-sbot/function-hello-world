f664880a2b3c043387b62a39deafffa88d8628f4
Dinis Cruz <dinis.cruz@owasp.org>
2020-12-11 10:52:49 +0000

    code refactoring

--- function_hello_world/aws/deploy/Deploy.py
+++ function_hello_world/aws/deploy/Deploy.py
@@ -1,15 +1,19 @@
-from osbot_aws.Globals import Globals
+from osbot_aws.AWS_Config import AWS_Config
+from osbot_aws.OSBot_Setup import OSBot_Setup
 from osbot_aws.apis.test_helpers.Temp_Aws_Roles import Temp_Aws_Roles
+from osbot_aws.helpers.Lambda_Package import Lambda_Package
 
 from function_hello_world.Env import Env
-
+from osbot_aws.deploy.Deploy_Lambda import Deploy_Lambda
 
 class Deploy:
 
-    def __init__(self):
+    def __init__(self, handler):
         Env()
-        self.lambda_name = "hello_world"
-        self.role_arn    = Temp_Aws_Roles().for_lambda_invocation__role_arn()
+        self.deploy_lambda = Deploy_Lambda(handler)
 
-    def check_aws_role(self):
-        pass
+    def now(self):
+        return self.deploy_lambda.update()
+
+    def invoke(self,params=None):
+        return self.deploy_lambda.lambda_invoke(params)
--- tests/aws/deploy/test_Deploy.py
+++ tests/aws/deploy/test_Deploy.py
@@ -1,29 +1,32 @@
+import os
+from pprint import pprint
 from unittest import TestCase
 
+from osbot_aws.AWS_Config import AWS_Config
+from osbot_aws.apis.IAM import IAM
 from osbot_aws.apis.Lambda import Lambda
 from osbot_aws.apis.test_helpers.Temp_Aws_Roles import Temp_Aws_Roles
+from osbot_aws.helpers.Test_Helper import Test_Helper
+from osbot_utils.utils.Files import folder_exists
 
 from function_hello_world.aws.deploy.Deploy import Deploy
+from function_hello_world.aws.lambdas.hello_world import run
 
 
 class test_Deploy(TestCase):
 
     def setUp(self) -> None:
-        self.deploy = Deploy()
+        self.deploy = Deploy(run)
+        #Test_Helper().check_aws_token()
+        self.aws_config = AWS_Config()
 
     def test__init__(self):
-        assert self.deploy.lambda_name == 'hello_world'
-        print()
-        print(self.deploy.role_arn)
 
-    def test_check_aws_role(self):
-        self.deploy.check_aws_role()
-        assert Temp_Aws_Roles().for_lambda_invocation__not_exists() is False
+        assert self.deploy.deploy_lambda.lambda_name == self.deploy.deploy_lambda.handler.__module__
+        assert self.deploy.deploy_lambda.role_arn == f"arn:aws:iam::{self.aws_config.aws_session_account_id()}:role/temp_role_for_lambda_invocation"
+        assert type(self.deploy.deploy_lambda.handler).__name__ == 'function'
 
-    # def test_deploy(self):
-    #     print()
-    #     self.deploy.check_aws_connection()
-    #
-    #     aws_lambda = Lambda().functions()
-    #     print(aws_lambda)
+    def test_now__invoke(self):
+        assert self.deploy.now()['status'] == 'ok'
+        assert self.deploy.invoke()        ==  'From lambda code, hello None'
 
